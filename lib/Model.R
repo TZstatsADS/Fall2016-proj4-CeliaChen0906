bf77c7bcd30945333a9b46a546ed02264b642522
Celia Chen <CeliaChen0906@users.noreply.github.com>
2016-11-18 18:33:33 -0500

    Add files via upload
    Model training and predicting code

--- lib/Model.R
+++ lib/Model.R
@@ -1,6 +1,21 @@
 ###Predicting Model
 
-
-for(i in 1:2350){
-  model[i]
+model <- list()
+for(p in 2:5001){
+  model[p] <- lm(lyr[,p] ~ song.features.df$bars + song.features.df$beats + song.features.df$sections1 + song.features.df$tatums)
 }
+
+
+pre <- list()
+for(n in 1:100){
+  for(i in 2:5001){
+    dir.h5 <- './TestSongFile100/'
+    files.list <- as.matrix(list.files(dir.h5, recursive = TRUE))
+    test.df <- get.features(files.list, dir.h5)
+   
+     pre[n,i] <-predict(model[i], newdata = test.df)
+  }
+}
+  
+ 
+write.csv(pre, "predict.csv")
